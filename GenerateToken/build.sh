068d1669062f6344c19a35bd1f61488dcc68a809
hxhwing <59213871+hxhwing@users.noreply.github.com>
2021-08-28 01:21:27 +0800

    Add terminate running enclave

--- GenerateToken/build.sh
+++ GenerateToken/build.sh
@@ -3,13 +3,19 @@ FILE=GenerateToken-demo.eif
 if [ -f "$FILE" ]; then
     rm $FILE
 fi
+
+RunningEnclave=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
+if [ -n "$RunningEnclave" ]; then
+	nitro-cli terminate-enclave --enclave-id $(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID");
+fi
+
 #docker rmi -f $(docker images -a -q)
-docker rmi GenerateToken-demo:latest
+docker rmi generatetoken-demo:latest
 pkill vsock-proxy
 
 
-docker build -t GenerateToken-demo:latest .
-nitro-cli build-enclave --docker-uri GenerateToken-demo:latest  --output-file GenerateToken-demo.eif > EnclaveImage.log
+docker build -t generatetoken-demo:latest .
+nitro-cli build-enclave --docker-uri generatetoken-demo:latest  --output-file GenerateToken-demo.eif > EnclaveImage.log
 
 vsock-proxy 8000 kms.ap-northeast-1.amazonaws.com 443 &
 
--- SignVerify/build.sh
+++ SignVerify/build.sh
@@ -3,13 +3,19 @@ FILE=SignVerify-demo.eif
 if [ -f "$FILE" ]; then
     rm $FILE
 fi
+
+RunningEnclave=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID")
+if [ -n "$RunningEnclave" ]; then
+	nitro-cli terminate-enclave --enclave-id $(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID");
+fi
+
 #docker rmi -f $(docker images -a -q)
-docker rmi SignVerify-demo:latest
+docker rmi signverify-demo:latest
 pkill vsock-proxy
 
 
-docker build -t SignVerify-demo:latest .
-nitro-cli build-enclave --docker-uri SignVerify-demo:latest  --output-file SignVerify-demo.eif > EnclaveImage.log
+docker build -t signverify-demo:latest .
+nitro-cli build-enclave --docker-uri signverify-demo:latest  --output-file SignVerify-demo.eif > EnclaveImage.log
 
 vsock-proxy 8000 kms.ap-northeast-1.amazonaws.com 443 &
 
